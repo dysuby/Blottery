38ae9e1f121e23ef6f5d576c981cfdf044131fee
Veg_chic <h_foryou@163.com>
2018-12-29 20:43:37 +0800

    add .text center

--- app/scripts/component.js
+++ app/scripts/component.js
@@ -1,10 +1,10 @@
 export function addlottery(address, due, pool, state = '') {
   $('#list').append(`
   <div class="row lottery">
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-vc">${address}</div>
-    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-vc">${due}</div>
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 text-vc">${pool}</div>
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-vc text-center">${address}</div>
+    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-vc text-center">${due}</div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 text-vc text-center">${pool}</div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 text-center">
       <buttun class="btn btn-success lottery-action-btn buy-btn ${state}">Buy</buttun>
       <buttun class="btn btn-primary lottery-action-btn award-btn ${state}">Award</buttun>
     </div>
